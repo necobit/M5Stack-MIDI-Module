f8294eeb73dfd2c909f51fcf6a1b40309168fb5a
necobit <dm@necobit.com>
2018-10-30 18:02:27 +0900

    Add a description

--- Arduino-IDE-Sketch/8bitNESSynth/8bitNESSynth.ino
+++ Arduino-IDE-Sketch/8bitNESSynth/8bitNESSynth.ino
@@ -1,3 +1,15 @@
+//NES Like Synth ver. 0.2
+//M5Stack用MIDIモジュールを利用したファミコン風音源です。
+//https://necobit.base.shop/items/14047506
+//MIDIモジュールはこちらで販売しています。
+//
+//シンセエンジンは@ina_aniさんのエンジンを参考に、
+//https://github.com/inajob/o-bako-esp32
+//NESの音源の仕組みはConnor Nishijimaさんのプログラムを参考にしました。
+//https://github.com/connornishijima/Cartridge
+//Lixie LabsのソースはGPLv3なので継承するか迷いましたが、
+//変数の定義を除くとソースはほぼ残っていないのでMITライセンスとします。
+
 #include <M5Stack.h>
 #include <driver/dac.h>
 #include <MIDI.h>
@@ -27,10 +39,10 @@ void setup() {
   M5.Lcd.setCursor(180, 90);
   M5.Lcd.print("Lixie Labs");
 
-  Tunes::outpin = 25;  //50kΩ程度の可変抵抗を持っている人はここを26に変更して↓
+  Tunes::outpin = 26;  //50kΩ程度の可変抵抗を持っている人はここを26に変更して↓
   dacWrite(Tunes::outpin, 0);
-  //ledcDetachPin(SPEAKER_PIN);   //ここのコメントアウトを↓
-  //pinMode(SPEAKER_PIN, INPUT);   //外して下さい。音量が下げられます。
+  ledcDetachPin(SPEAKER_PIN);   //ここのコメントアウトを↓
+  pinMode(SPEAKER_PIN, INPUT);   //外して下さい。音量が下げられます。
   tunes.init();
   M5.Speaker.setVolume(1);
 
